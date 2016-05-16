3759bac765c60e8ab927a50bc6904db0c3d7cea6
Your Name <jaknollmeyer@gmail.com>
2016-05-16 10:37:58 -0400

    Add script to determine positivity/negativity of candidates debate talk

---
+++ candidate_sentiment.py
@@ -0,0 +1,57 @@
+# scrapes raw transcripts to determine the overall sentiment of a candidate
+
+import os
+# import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
+from helper_functions import transcript_to_candidate_lines
+
+
+def word_feats(words):
+    return dict([(word, True) for word in words])
+
+
+directories = ["gop_transcripts", "dem_transcripts"]
+DEMOCRAT_CANDIDATES = ["SANDERS", "CLINTON", "O'MALLEY"]
+GOP_CANDIDATES = ["BUSH", "CARSON", "CHRISTIE", "CRUZ", "KASICH", "TRUMP"]
+
+candidateList = DEMOCRAT_CANDIDATES + GOP_CANDIDATES
+transcripts = (transcript
+               for directory in directories
+               for fileName in os.listdir(directory)
+               for transcript in open(directory + '/' + fileName).readlines())
+
+lineList = transcript_to_candidate_lines(transcripts, candidateList)
+
+negids = movie_reviews.fileids('neg')
+posids = movie_reviews.fileids('pos')
+
+negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg')
+            for f in negids]
+posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos')
+            for f in posids]
+
+trainfeats = negfeats + posfeats
+
+classifier = NaiveBayesClassifier.train(trainfeats)
+# print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+# classifier.show_most_informative_features()
+
+sentimentCount = dict((candidate, {'pos': 0, 'neg': 0})
+                      for candidate in candidateList)
+for candidate in candidateList:
+    print "Generating sentiment for " + candidate
+    for line in lineList[candidate]:
+
+        testingWordDict = {}
+        for word in line:
+            testingWordDict[word] = True
+
+        prediction = classifier.classify(testingWordDict)
+        sentimentCount[candidate][prediction] += 1
+    percentage = 100 * sentimentCount[candidate]['pos'] / \
+        float(sentimentCount[candidate]['pos'] +
+              sentimentCount[candidate]['neg'])
+    print "Positive lines: " + str(sentimentCount[candidate]['pos'])
+    print "Negative lines: " + str(sentimentCount[candidate]['neg'])
+    print "Postive %%: %0.2f" % percentage
